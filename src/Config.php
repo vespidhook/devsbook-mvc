7e7f2978e30eba80b611cc2ad289a5c192aa7833
Bruno Alves <vespidhook@gmail.com>
2020-11-23 16:59:06 -0300

    iniciando devsbook

--- src/Config.php
+++ src/Config.php
@@ -2,11 +2,11 @@
 namespace src;
 
 class Config {
-    const BASE_DIR = '/b7web/PHP/MODULO11/devsbook/public';
+    const BASE_DIR = '/mvc/public';
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
-    const DB_DATABASE = 'devsbook';
+    const DB_DATABASE = 'test';
     CONST DB_USER = 'root';
     const DB_PASS = '';
 
--- src/routes.php
+++ src/routes.php
@@ -4,9 +4,5 @@ use core\Router;
 $router = new Router();
 
 $router->get('/', 'HomeController@index');
-
-$router->get('/login', 'LoginController@signin');
-$router->post('/login', 'LoginController@signinAction');
-
-$router->get('/cadastro', 'LoginController@signup');
-$router->post('/cadastro', 'LoginController@signupAction');
+$router->get('/sobre/{nome}', 'HomeController@sobreP');
+$router->get('/sobre', 'HomeController@sobre');
